93aa81c484ff3aaca26c966e743eeef11f0ed9da
reDawsonLin <redawson357@gmail.com>
2021-11-08 23:25:08 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-// 載入 express 並建構應用程式伺服器
+//step 載入 express 並建構應用程式伺服器
 const express = require('express')
-const mongoose = require('mongoose') // 載入 mongoose
-
-
 const app = express()
+
+// 設定首頁路由
+// app.get('/', (req, res) => {
+//   res.render('index')
+// })
+
+// 設定 port 3000
+app.listen(3000, () => {
+  console.log('App is running on http://localhost:3000')
+})
+
+
+
+//step mongoose
+const mongoose = require('mongoose') // 載入 mongoose
 
 mongoose.connect('mongodb://localhost/todo-list', {
   useNewUrlParser: true,
@@ -23,18 +35,10 @@ db.once('open', () => {
 
 
 
-// 設定首頁路由
-app.get('/', (req, res) => {
-  res.render('index')
-})
 
-// 設定 port 3000
-app.listen(3000, () => {
-  console.log('App is running on http://localhost:3000')
-})
 
 //---
-//handlebars
+//step handlebars
 const exphbs = require('express-handlebars')
 
 //建立一個名為 hbs的樣板引擎，並傳入 exphbs與相關參數
@@ -42,6 +46,19 @@ app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 //設定 extname: '.hbs'，才能把預設的長檔名改寫成短檔名。
 
 
-
 //啟用樣板引擎 hbs
 app.set('view engine', 'hbs')
+
+
+//---
+//step CRUD action
+const Todo = require('./models/todo')
+
+app.get('/', (req, res) => {
+  Todo.find() // 取出 Todo model 裡的所有資料
+    .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
+    .then(todos => res.render('index', {
+      todos
+    })) // 將資料傳給 index 樣板
+    .catch(error => console.error(error)) // 錯誤處理
+})
