7ef2b088a500b4126fe45e46c674115813893572
reDawsonLin <redawson357@gmail.com>
2021-11-08 23:52:14 +0800

    feat: add create todo function

--- app.js
+++ app.js
@@ -62,3 +62,24 @@ app.get('/', (req, res) => {
     })) // 將資料傳給 index 樣板
     .catch(error => console.error(error)) // 錯誤處理
 })
+
+
+//---
+//step Create todo
+app.get('/todos/new', (req, res) => {
+  return res.render('new')
+})
+
+app.post('/todos', (req, res) => {
+  const name = req.body.name
+  return Todo.create({ name })
+    .then((() => res.redirect('/')))
+    .catch(error => console.log(error))
+})
+
+/// 引用 body-parser
+const bodyParser = require('body-parser')
+// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
