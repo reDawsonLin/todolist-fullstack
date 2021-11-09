61717d3eea1663c7d59ae2dfefdfaa0e6f2f5b95
reDawsonLin <redawson357@gmail.com>
2021-11-09 11:30:56 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -12,26 +12,9 @@ app.listen(3000, () => {
   console.log('App is running on http://localhost:3000')
 })
 
+require('./config/mongoose')
 
 
-//step mongoose
-const mongoose = require('mongoose') // 載入 mongoose
-
-mongoose.connect('mongodb://localhost/todo-list', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}) // 設定連線到 mongoDB
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
 
 
 
--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,16 +1,6 @@
-const mongoose = require('mongoose')
 const Todo = require('../todo') // 載入 todo model
+const db = require('../../config/mongoose')
 
-mongoose.connect('mongodb://localhost/todo-list', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-
-const db = mongoose.connection
-
-db.on('error', () => {
-  console.log('mongodb error!')
-})
 
 db.once('open', () => {
   console.log('mongodb connected!')
